4ed78ae0364249627ef94383637d1614154a58f9
icelenaugust <dingyoujia0823@gmail.com>
2021-01-08 13:47:00 +0800

    profile updated

--- screens/Profile.js
+++ screens/Profile.js
@@ -14,42 +14,42 @@ var hasUpdated = false;
 
 export default class Login extends Component {
 
-    writeUserData(userId, imageUrl) {
-        firebase.database().ref('users/' + userId).set({
-          profile_picture : imageUrl
-        });
-    }
+    // writeUserData(userId, imageUrl) {
+    //     firebase.database().ref('users/' + userId).set({
+    //       profile_picture : imageUrl
+    //     });
+    // }
 
-    uploadImage(imageUrl) {
-        var storageRef = storage.ref();
-        var imagesRef = storageRef.child('newProfilePicture');
-        imagesRef.putFile(imageUrl);
-    }
+    // uploadImage(imageUrl) {
+    //     var storageRef = storage.ref();
+    //     var imagesRef = storageRef.child('newProfilePicture');
+    //     imagesRef.putFile(imageUrl);
+    // }
 
 
-    updateProfilePic = async () => {
-        hasUpdated = true;
-        if (Platform.OS !== 'web') {
-            const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-            if (status !== 'granted') {
-              alert('Sorry, we need camera roll permissions to make this work!');
-            } else {
-                let result = await ImagePicker.launchImageLibraryAsync({
-                    mediaTypes: ImagePicker.MediaTypeOptions.All,
-                    allowsEditing: true,
-                    aspect: [1, 1],
-                    quality: 1,
-                });
-                if (!result.cancelled) {
-                    updatedURI = result.uri;
-                    //this.uploadImage(imageUrl);
-                    //this.writeUserData(uid, )
+    // updateProfilePic = async () => {
+    //     hasUpdated = true;
+    //     if (Platform.OS !== 'web') {
+    //         const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    //         if (status !== 'granted') {
+    //           alert('Sorry, we need camera roll permissions to make this work!');
+    //         } else {
+    //             let result = await ImagePicker.launchImageLibraryAsync({
+    //                 mediaTypes: ImagePicker.MediaTypeOptions.All,
+    //                 allowsEditing: true,
+    //                 aspect: [1, 1],
+    //                 quality: 1,
+    //             });
+    //             if (!result.cancelled) {
+    //                 updatedURI = result.uri;
+    //                 //this.uploadImage(imageUrl);
+    //                 //this.writeUserData(uid, )
 
-                }
+    //             }
 
-            }
-        }
-    };
+    //         }
+    //     }
+    // };
 
 
     render() {
@@ -81,10 +81,7 @@ export default class Login extends Component {
             <SafeAreaView style={styles.container}>
                 <View style={{ alignSelf: "center", marginTop: "20%" }}>
                     <View style={styles.profileImage}>
-                        <Image source={user ? hasUpdated
-                                                ? {uri: updatedURI}
-                                                : {url: `${photoURL}`}
-                                            
+                        <Image source={user ? {url: `${photoURL}`}
                                             : require('../images/no-profile-pic.jpg')} style={styles.image} resizeMode="cover"></Image>
                     </View>
                 </View>
@@ -102,7 +99,7 @@ export default class Login extends Component {
                             style={styles.button}
                             onPress={() => this.props.navigation.navigate('Friends')}
                         >
-                            <Text style={[styles.text, {fontSize:24} ]}> 30
+                            <Text style={[styles.text, {fontSize:24} ]}>30
                             </Text>
                         </TouchableOpacity>
                         <Text style={[styles.text, styles.subText]}>Friends</Text>

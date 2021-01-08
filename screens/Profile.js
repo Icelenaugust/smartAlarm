16be054243a330aafe222df85889b42a8da99bd1
Zong Yu <hanzongyu@live.com>
2021-01-09 02:29:51 +0800

    change profile picture feature

--- screens/Profile.js
+++ screens/Profile.js
@@ -13,12 +13,17 @@ import firebase from "firebase";
 import Constants from "expo-constants";
 import * as Notifications from "expo-notifications";
 import * as Permissions from "expo-permissions";
+import * as ImagePicker from "expo-image-picker";
+import { v4 as uuidv4 } from "uuid";
 
 var updatedURI;
-var uid;
 var hasUpdated = false;
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLoading: true };
+  }
 
   registerForPushNotificationsAsync = async () => {
     if (Constants.isDevice) {
@@ -29,104 +34,124 @@ export default class Login extends Component {
       if (existingStatus !== "granted") {
         const { status } = await Permissions.askAsync(
           Permissions.NOTIFICATIONS
-          );
-          finalStatus = status;
-        }
-        if (finalStatus !== "granted") {
-          alert("Failed to get push token for push notification!");
-          return;
-        }
-        // Get the token that uniquely identifies this device
-        let token = await Notifications.getExpoPushTokenAsync();
-        console.log(token.data);
-        // POST the token to your backend server from where you can retrieve it to send push notifications.
-        firebase
-          .database()
-          .ref("users/" + this.currentUser.uid + "/push_token")
-          .set(token);
-      } else {
-        alert("Must use physical device for Push Notifications");
+        );
+        finalStatus = status;
       }
-  
-      if (Platform.OS === "android") {
-        Notifications.setNotificationChannelAsync("default", {
-          name: "default",
-          importance: Notifications.AndroidImportance.MAX,
-          vibrationPattern: [0, 250, 250, 250],
-          lightColor: "#FF231F7C",
-        });
+      if (finalStatus !== "granted") {
+        alert("Failed to get push token for push notification!");
+        return;
       }
-    };
-    async componentDidMount() {
-        this.currentUser = await firebase.auth().currentUser;
-        await this.registerForPushNotificationsAsync();
-      }
+      // Get the token that uniquely identifies this device
+      let token = await Notifications.getExpoPushTokenAsync();
+      console.log(token.data);
+      // POST the token to your backend server from where you can retrieve it to send push notifications.
+      firebase
+        .database()
+        .ref("users/" + this.currentUser.uid + "/push_token")
+        .set(token);
+    } else {
+      alert("Must use physical device for Push Notifications");
+    }
 
-
-  // writeUserData(userId, imageUrl) {
-  //     firebase.database().ref('users/' + userId).set({
-  //       profile_picture : imageUrl
-  //     });
-  // }
-
-  // uploadImage(imageUrl) {
-  //     var storageRef = storage.ref();
-  //     var imagesRef = storageRef.child('newProfilePicture');
-  //     imagesRef.putFile(imageUrl);
-  // }
-
-  // updateProfilePic = async () => {
-  //     hasUpdated = true;
-  //     if (Platform.OS !== 'web') {
-  //         const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-  //         if (status !== 'granted') {
-  //           alert('Sorry, we need camera roll permissions to make this work!');
-  //         } else {
-  //             let result = await ImagePicker.launchImageLibraryAsync({
-  //                 mediaTypes: ImagePicker.MediaTypeOptions.All,
-  //                 allowsEditing: true,
-  //                 aspect: [1, 1],
-  //                 quality: 1,
-  //             });
-  //             if (!result.cancelled) {
-  //                 updatedURI = result.uri;
-  //                 //this.uploadImage(imageUrl);
-  //                 //this.writeUserData(uid, )
-
-  //             }
-
-  //         }
-  //     }
-  // };
+    if (Platform.OS === "android") {
+      Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+  };
   async componentDidMount() {
     this.currentUser = await firebase.auth().currentUser;
+    this.setState({ user: this.currentUser, isLoading: false });
+    this.retrieveName(this.currentUser);
+    this.retrievePic(this.currentUser);
     await this.registerForPushNotificationsAsync();
   }
-  render() {
-    var user = firebase.auth().currentUser;
-    var storage = firebase.storage();
-    var displayName;
-    var photoURL;
 
-    if (user != null) {
-      uid = user.uid;
-
-      var nameRef = firebase.database().ref("users/" + uid + "/first_name");
-      nameRef.on("value", (snapshot) => {
-        const data = snapshot.val();
-        displayName = data;
+  writeUserData(userId, imageUrl) {
+    firebase
+      .database()
+      .ref("users/" + userId)
+      .update({
+        profile_picture: imageUrl,
       });
+    this.setState({ profile_image: imageUrl });
+  }
 
-      var profilePicRef = firebase
-        .database()
-        .ref("users/" + uid + "/profile_picture");
-      profilePicRef.on("value", (snapshot) => {
-        const data = snapshot.val();
-        photoURL = data;
-      });
-    } else {
-      displayName = "No User";
+  uploadImage = async (imageUri) => {
+    const path = `users/${this.uid}/profile_picture_uri`;
+
+    return new Promise(async (res, rej) => {
+      const response = await fetch(imageUri);
+      const file = await response.blob();
+
+      let upload = firebase.storage().ref(path).put(file);
+
+      upload.on(
+        "state_changed",
+        (snapshot) => {},
+        (err) => {
+          rej(err);
+        },
+        async () => {
+          const url = await upload.snapshot.ref.getDownloadURL();
+          res(url);
+        }
+      );
+    });
+  };
+
+  updateProfilePic = async (user) => {
+    hasUpdated = true;
+    if (Platform.OS !== "web") {
+      const {
+        status,
+      } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (status !== "granted") {
+        alert("Sorry, we need camera roll permissions to make this work!");
+      } else {
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.All,
+          allowsEditing: true,
+          aspect: [1, 1],
+          quality: 1,
+        });
+        if (!result.cancelled) {
+          updatedURI = result.uri;
+          // console.log(result);
+          const url = await this.uploadImage(updatedURI);
+          this.writeUserData(user.uid, url);
+        }
+      }
     }
+  };
+
+  retrievePic = (user) => {
+    firebase
+      .database()
+      .ref("users/" + user.uid + "/profile_picture")
+      .on("value", (snapshot) => {
+        const data = snapshot.val();
+        // console.log(data);
+        this.setState({ profile_image: data });
+      });
+  };
+
+  retrieveName = (user) => {
+    console.log(user);
+    firebase
+      .database()
+      .ref("users/" + user.uid + "/first_name")
+      .on("value", (snapshot) => {
+        const data = snapshot.val();
+        this.setState({ user_name: data });
+      });
+  };
+
+  render() {
+    var user = this.state.user;
 
     return (
       <SafeAreaView style={styles.container}>
@@ -135,7 +160,7 @@ export default class Login extends Component {
             <Image
               source={
                 user
-                  ? { url: `${photoURL}` }
+                  ? { url: this.state.profile_image }
                   : require("../images/no-profile-pic.jpg")
               }
               style={styles.image}
@@ -146,7 +171,7 @@ export default class Login extends Component {
 
         <View style={styles.infoContainer}>
           <Text style={[styles.text, { fontWeight: "200", fontSize: 36 }]}>
-            {user ? displayName : "No User"}
+            {user ? this.state.user_name : "No User"}
           </Text>
         </View>
 
@@ -164,10 +189,10 @@ export default class Login extends Component {
           </View>
         </View>
         <View style={{ alignSelf: "center", margin: 10 }}>
-          {/* <Button
+          <Button
             title="Change Profile Picture"
-            onPress={() => this.updateProfilePic()}
-          /> */}
+            onPress={() => this.updateProfilePic(user)}
+          />
         </View>
       </SafeAreaView>
     );
